50a5a4c4bbe7d19883146128a69159d0d5c46966
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-07 18:31:01 +0530

    Update first.java

--- first.java
+++ first.java
@@ -1,1 +1,40 @@
 //Hello this is my first program online...
+public class HelloWorld{
+  public static void main(String args[]){
+    System.out.println("Hello from Yash");
+    System.out.println("****");
+    System.out.println("***");
+    System.out.println("**");
+    System.out.println("*");
+
+    int a= 10 ;
+    int b= 20;
+    String name="Yash Tambe";
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(name);
+    
+    //Data Types in Java (Primitive)
+    
+    byte d = 8; 
+    System.out.println(b);
+    char ch="A";
+    System.out.println(ch);
+    boolean var = false;
+    float price = 10.5;
+    int number = 25;
+    //long
+    //double
+    short n = 240;
+    
+    int x = 10;
+    int y = 20;
+    int sum = a+b;
+    System.out.println(sum);
+    
+    
+    
+    
+    
+    }
+}
