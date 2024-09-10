14590abff3ea82758c1cdd55d26e00a0ec8e332e
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-10 14:40:56 +0530

    Update statements.java

--- statements.java
+++ statements.java
@@ -1,2 +1,19 @@
 //Hello , We are going to learn Conditional Statements , Lets Start:-
 
+public  class statements {
+    
+        public static void main(String[] args) {
+        int age = 17;
+        /* This is an If-Else Conditional statements... */
+         if (age >=21){
+            System.out.println("Your are an Adult");
+         }
+         if (age >=15){
+            System.out.println("Your are  a Teen");
+         }
+         else{
+            System.out.println("Your are still not Adult");
+         }
+    }
+}
+ 
