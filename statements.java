643f89b632a89d934f3b360c90a5f757ccde0b7e
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-12 01:15:18 +0530

    Update statements.java

--- statements.java
+++ statements.java
@@ -1,4 +1,3 @@
-//Hello , We are going to learn Conditional Statements , Lets Start:-
 
 public  class statements {
     
@@ -14,6 +13,31 @@ public  class statements {
          else{
             System.out.println("Your are still not Adult");
          }
+
+         int A = 100;
+         int B = 20;
+         if(A >= B){
+            System.out.println("A is Greater then B");
+         }
+         else{
+            System.out.println("B is Greater then A");
+         }
+         /*Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();*/int n= 20;
+
+         if( n % 2 == 0 ){
+            System.out.println("The following number is Even"); 
+            }
+            else{
+               System.out.println("The following nummber is odd");
+            }
+            int Aage = 21 
+            ;
+            if(Aage>= 18){System.out.println("You are an Adult");}
+            else if(Aage >= 13 && Aage <=18){System.out.println("You are a Teen");}
+            else{System.out.println("You are A Kid");}
+
     }
+
 }
  
