6654eaae9e61f3ff2b408a9893fb150964952e54
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-13 13:09:38 +0530

    Add files via upload

---
+++ large3.java
@@ -0,0 +1,14 @@
+public class large3 {
+    public static void main(String[] args) {
+        int a = 1000 , b = 200 , c = 30;
+        if( (a > b) && (a > c)) {
+            System.out.println("A is the Greatest");
+        }
+        else if ( b > c ) {
+            System.out.println("B is the Greatest");
+        }
+        else {
+            System.out.println("C is the greatest");
+        }
+    }
+}
