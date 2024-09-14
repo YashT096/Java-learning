d7933e72de6f8b24003f789120fd9dc986a3eb63
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-14 23:22:09 +0530

    Update statements.java

--- statements.java
+++ statements.java
@@ -37,6 +37,30 @@ public  class statements {
             else if(Aage >= 13 && Aage <=18){System.out.println("You are a Teen");}
             else{System.out.println("You are A Kid");}
 
+
+            //Ternary Operator
+
+            int number = 34;
+            String type = ((number%2) ==0)? "Even" : "Odd";
+            System.out.println(type);
+            
+
+            int grade = 9;
+            String status = grade >=35 ? "Pass" : " Fail";
+            System.out.println(status);
+
+        //Switch Statement
+        int pointer = 6;
+
+if (pointer == 10) {
+    System.out.println("Good !");
+} else if (pointer == 5) {
+    System.out.println("Distinction!!");
+} else {
+    System.out.println("Fail!");
+    throw new AssertionError();
+}
+
     }
 
 }
