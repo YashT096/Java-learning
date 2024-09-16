c6d380529a11f3c723949ad070d43237ef27a6c0
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-16 23:43:00 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -1,12 +1,11 @@
 
-import java.util.Scanner;
 
 public class loops {
     public static void main(String[] args) {
      // while loop ; for Loop ; do while loop
 
      //While loop
-     int n = 3;
+     int n = 4 ;
      while (n>5 ) {
         System.out.println("Hello World"); 
          
@@ -23,15 +22,22 @@ public class loops {
         //counter++;
     //}
     
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter a range:");
-       int counter = 1;
-       int range=sc.nextInt();
+   // Scanner sc = new Scanner(System.in);
+    //System.out.println("Enter a range:");
+      // int counter = 1;
+       //int range=sc.nextInt();
       
-       while( counter <= range ){
-        System.out.println(counter + "" );
-        counter++;
-       }
-       sc.close();
+       //while( counter <= range ){
+        //System.out.println(counter + "" );
+        //counter++;
+       //}
+       //sc.close();
+
+       // For Loop
+       //int = 1 ;
+      for(int i = 1; i<=10 ; i++ );
+      System.out.println("Hello World");
+
+      
     }
 }
