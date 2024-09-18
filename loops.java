c4e2baaa4fc03e88e1cc69068e6089a154729108
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-18 18:02:44 +0530

    Update loops.java

--- loops.java
+++ loops.java
@@ -1,4 +1,9 @@
 
+import java.util.Scanner;
+
+
+
+
 
 public class loops {
     public static void main(String[] args) {
@@ -35,9 +40,76 @@ public class loops {
 
        // For Loop
        //int = 1 ;
-      for(int i = 1; i<=10 ; i++ );
-      System.out.println("Hello World");
+      for (int i = 1; i<=10; i++ ){
+      System.out.println("Hello Its Yash!");
+      }
+   // Reverse of a number
+    int random = 123456789;
 
+  while(random >0){
+    int lastdigit = random % 10;
+    System.out.print(lastdigit+"");
+    random = random / 10;
+
+  }
+  System.out.println("");
+
+  int k = 124345;
+  int rev = 0;
+  while(k>0){
+    int last = k%10;
+    rev = (rev*10) + last;
+    k= k/10;
+
+  }    
+  System.out.println(rev);
+
+  //do-While loop
+int counter =1 ;
+  do { 
+      System.out.println("Hey! ,Hello");
+      counter++;
+  } while (counter<=10);
+  
+
+ //  Breal statement :- to break the loop 
+ 
+ for(int j =1;j<=5;j++){
+ if(j ==3){
+  break;}
+  System.out.println(j); }
+    
+    System.out.println("I am out of the loop");
+
+  Scanner sc = new Scanner (System.in);
+  
+ do {
+      System.out.println("Enter Your Number:-");
+       int p = sc.nextInt();
+
+  if (p % 10 == 0 ){
+    break;
+  }
+
+  System.out.println(p);
+}
+ while (true); 
+ sc.close();
       
+ // continue Statement
+
+ for(int q = 1; q <=5 ;q++){
+  if(q == 3){
+    continue;
+  }System.out.println(q);
+ }
+ 
+
+ for (int h = 1; h <= 25;h ++ ){
+  if (h % 5 == 0){
+    continue;
+  }
+  System.out.println(h);
+ }
     }
 }
