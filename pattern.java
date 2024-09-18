9450a25afcf829806d1dd60f32925dae4f730cab
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-18 18:03:32 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,15 @@
+public class pattern {
+    public static void main(String[] args) {
+        //Square Pattern
+       for(int line=1; line<=4 ; line++){
+        System.out.println("****");
+       }
+   // Sqaure Pattern by using while loop
+       int sq =1;
+       while(sq<=4){
+        System.out.println("****");
+        sq++;
+       }
+       
+    }
+}
