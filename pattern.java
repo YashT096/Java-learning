f30384fcd6d93b299e61ed4ab3643692d1bd7c19
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-20 11:52:24 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,9 +1,9 @@
 public class pattern {
     public static void main(String[] args) {
         //Square Pattern
-       for(int line=1; line<=4 ; line++){
-        System.out.println("****");
-       }
+      // for(int line=1; line<=4 ; line++){
+        //System.out.println("****");
+       //}
    // Sqaure Pattern by using while loop
        int sq =1;
        while(sq<=4){
@@ -11,5 +11,21 @@ public class pattern {
         sq++;
        }
        
+       // Star Pattern(nested Loop)
+       for(int line =1;line<=6;line++){
+        for(int star=1; star<=line ; star++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+       
+      //Inverted Stars
+      for(int i=1; i<=5;i++){
+        for(int str=str-i+1; str>=i;str++){
+            System.out.println("*");
+        }
+        System.out.println();
+      }
+        
     }
 }
