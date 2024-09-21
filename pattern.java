a0689101cb8ef21f919f257f87d8429c2976cb7e
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-21 16:24:51 +0530

    Update pattern.java

--- pattern.java
+++ pattern.java
@@ -1,3 +1,6 @@
+
+import javax.sound.sampled.Line;
+
 public class pattern {
     public static void main(String[] args) {
         //Square Pattern
@@ -19,13 +22,20 @@ public class pattern {
         System.out.println();
        }
        
-      //Inverted Stars
+      /**Inverted Stars
       for(int i=1; i<=5;i++){
         for(int str=str-i+1; str>=i;str++){
             System.out.println("*");
         }
-        System.out.println();
-      }
-        
+        System.out.println();**/
+      };
+        //Half Pyramid pattern
+        for(int line=1; line<=5; line++){
+          for (int number=1;number<=line; number++){
+          System.out.println(number);
+        }
+         System.out.println();
     }
+     
+
 }
